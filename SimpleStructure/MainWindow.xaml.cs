83bb54b4dc5ab71ff32124538da7fede85808c41
freemanbach <flo@radford.edu>
2023-04-04 16:28:56 -0400

    Simple Treview in Code

---
+++ SimpleStructure/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleStructure {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private void onLoad(object sender, RoutedEventArgs e) {
+            TreeViewItem treeItem = null;
+
+            // North America
+            treeItem = new TreeViewItem();
+            treeItem.Header = "Sample Countries in World";
+
+            treeItem.Items.Add(new TreeViewItem() { Header = "germany" });
+            treeItem.Items.Add(new TreeViewItem() { Header = "spain" });
+            treeItem.Items.Add(new TreeViewItem() { Header = "australia" });
+
+            tview.Items.Add(treeItem);
+        }
+    }
+}
