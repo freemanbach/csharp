e473076c74c6b84d8bb17b9bed675d6256b09dff
freeman <aschenbach@gmail.com>
2023-07-08 13:29:53 -0400

    Initial commit

---
+++ Hashing/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using System.Collections.Generic;
+using System.Linq;
+// using System.Net.Http;
+// using System.Threading;
+// using System.Threading.Tasks;
+
+namespace Hashing {
+
+    public class Program {
+
+        public static void Main(string [] args) {
+        
+        }
+    }
+}
