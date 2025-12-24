6d0d5a0ce9aa98a5165c0e2e7bdea785c8438de6
Freemanbach <flo@radford.edu>
2025-12-24 15:32:42 -0800

    refactor some code

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -1,81 +1,118 @@
 ﻿using System;
+using System.IO.Pipes;
 using System.Linq;
 
 namespace Calc {
     class Program {
-        public static void Main(string[] args) {
-            double ans = 0.00;
-            string t = args[0];
-            char symbol;
-            int d1 =0, d2 = 0;
 
-            if ( !t.Any(Char.IsLetter) == false) {
+        public static Boolean chkData(string t1, string t2) {
+           if (t2 == "" || t2 == "") {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        public static double compute(string t) {
+
+            int d1 = 0, d2 = 0;
+            double ans = 0.00;
+            string[] dt;
+
+            if (!t.Any(Char.IsLetter) == false) {
                 Console.WriteLine("Invalid Characters!");
                 Environment.Exit(0);
             }
-
-            if (args.Length < 1 ) {
-                Console.WriteLine("Insufficent arguments!");
-                Environment.Exit(0);
-            } else if (args.Length == 3) {
-                symbol = Convert.ToChar(args[1].Trim());
-
-                if (args[0].Trim() == "" || args[2].Trim() == "") {
+            if (t.Contains("+")) {
+                dt = t.Split('+');
+                if (chkData(dt[0], dt[1]) == true ) {
                     Console.WriteLine("Insufficent arguments!");
                     Environment.Exit(0);
                 }
-
-                d1 = Convert.ToInt32(args[0].Split('.')[0]);
-                d2 = Convert.ToInt32(args[2].Split('.')[0]);
-                if (symbol == '+') { ans = Convert.ToDouble(args[0].Trim()) + Convert.ToDouble(args[2].Trim()); }
-                if (symbol == '-') { ans = Convert.ToDouble(args[0].Trim()) - Convert.ToDouble(args[2].Trim()); }
-                if (symbol == '*') { ans = Convert.ToDouble(args[0].Trim()) * Convert.ToDouble(args[2].Trim()); }
-                if (symbol == '/') { ans = Convert.ToDouble(args[0].Trim()) / Convert.ToDouble(args[2].Trim()); }
-                if (symbol == '%') { ans = d1 % d2; }
-            } else {
-                string[] data;
-                if (t.Contains("+")) {
-                    data = t.Split('+');
-                    ans = Convert.ToDouble(data[0]) + Convert.ToDouble(data[1]);
+                ans = Convert.ToDouble(dt[0]) + Convert.ToDouble(dt[1]);
+            } else if (t.Contains("-")) {
+                dt = t.Split('-');
+                if (chkData(dt[0], dt[1]) == true) {
+                    Console.WriteLine("Insufficent arguments!");
+                    Environment.Exit(0);
                 }
-                else if (t.Contains("-")) {
-                    data = t.Split('-');
-                    ans = Convert.ToDouble(data[0]) - Convert.ToDouble(data[1]);
+                ans = Convert.ToDouble(dt[0]) - Convert.ToDouble(dt[1]);
+            } else if (t.Contains("/")) {
+                dt = t.Split('/');
+                if (chkData(dt[0], dt[1]) == true) {
+                    Console.WriteLine("Insufficent arguments!");
+                    Environment.Exit(0);
                 }
-                else if (t.Contains("/")) {
-                    data = t.Split('/');
-                    ans = Convert.ToDouble(data[0]) / Convert.ToDouble(data[1]);
+                ans = Convert.ToDouble(dt[0]) / Convert.ToDouble(dt[1]);
+            } else if (t.Contains("*")) {
+                dt = t.Split('*');
+                if (chkData(dt[0], dt[1]) == true) {
+                    Console.WriteLine("Insufficent arguments!");
+                    Environment.Exit(0);
                 }
-                else if (t.Contains("*")) {
-                    data = t.Split('*');
-                    ans = Convert.ToDouble(data[0]) * Convert.ToDouble(data[1]);
-                } else if (t.Contains("%")) {
-                    data = t.Split('%');
-                    if (data[0] == "" || data[1] == "") {
-                        Console.WriteLine("Insufficent arguments!");
-                        Environment.Exit(1);
+                ans = Convert.ToDouble(dt[0]) * Convert.ToDouble(dt[1]);
+            } else if (t.Contains("%")) {
+                dt = t.Split('%');
+                if (chkData(dt[0], dt[1]) == true) {
+                    Console.WriteLine("Insufficent arguments!");
+                    Environment.Exit(0);
+                } else {
+                    if (dt[0].Contains('.') && dt[1].Contains('.')) {
+                        d1 = Convert.ToInt32(dt[0].Split('.')[0]);
+                        d2 = Convert.ToInt32(dt[1].Split('.')[0]);
+                        ans = d1 % d2;
+                    } else if (dt[0].Contains('.')) {
+                        d1 = Convert.ToInt32(dt[0].Split('.')[0]);
+                        d2 = Convert.ToInt32(dt[1]);
+                        ans = d1 % d2;
+                    } else if (dt[1].Contains('.')) {
+                        d1 = Convert.ToInt32(dt[0]);
+                        d2 = Convert.ToInt32(dt[1].Split('.')[0]);
+                        ans = d1 % d2;
                     } else {
-                        if (data[0].Contains('.') && data[1].Contains('.')) {
-                            d1 = Convert.ToInt32(data[0].Split('.')[0]);
-                            d2 = Convert.ToInt32(data[1].Split('.')[0]);
-                            ans = d1 % d2;
-                        } else if (data[0].Contains('.')) {
-                            d1 = Convert.ToInt32(data[0].Split('.')[0]);
-                            d2 = Convert.ToInt32(data[1]);
-                            ans = d1 % d2;
-                        } else if (data[1].Contains('.')) {
-                            d1 = Convert.ToInt32(data[0]);
-                            d2 = Convert.ToInt32(data[1].Split('.')[0]);
-                            ans = d1 % d2;
-                        } else {
-                            d1 = Convert.ToInt32(data[0]);
-                            d2 = Convert.ToInt32(data[1]);
-                            ans = d1 % d2;
-                        }
+                        d1 = Convert.ToInt32(dt[0]);
+                        d2 = Convert.ToInt32(dt[1]);
+                        ans = d1 % d2;
                     }
                 }
             }
-            Console.WriteLine($" {String.Format("{0:F5}",ans)}" );
+            return ans;
+        }
+        public static void Main(string[] args) {
+
+            double fans = 0.00; 
+            string t = "";
+
+            // menu
+            if (args.Length <= 0) {
+                Console.WriteLine();
+                Console.WriteLine("Insufficient Arguments....          \n\n");
+                Console.WriteLine("Ex:                                     ");
+                Console.WriteLine("calc.exe  34+45.99                      ");
+                Console.WriteLine("calc.exe  34-45.99                      ");
+                Console.WriteLine("calc.exe  34*45.99                      ");
+                Console.WriteLine("calc.exe  34/45.99                      ");
+                Console.WriteLine("calc.exe  34%45                         ");
+                Console.WriteLine();
+                Environment.Exit(0);
+            } else if (args.Length == 1) {
+
+                string tmp = args[0];
+
+                if (tmp.Contains('+') || tmp.Contains('-') || tmp.Contains('*') ||
+                     tmp.Contains('/') || tmp.Contains('%')) {
+                    fans = compute(args[0]);
+                } else {
+                    Console.WriteLine("Insufficient Arguments");
+                    Environment.Exit(0);
+                }
+            } else {
+                // combine them
+                foreach (string tmp in args) {
+                    t += tmp;
+                }
+                fans = compute(t);
+            }
+            Console.WriteLine($"{String.Format("{0:F5}", fans)}");
         }
     }
 }
